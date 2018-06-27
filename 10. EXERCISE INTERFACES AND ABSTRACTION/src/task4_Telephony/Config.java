bbc07d879720a62befb80b2e1f9c7837796da3c2
Bebrevski <stefan.bebrevski89@gmail.com>
2018-06-27 18:11:52 +0300

    Course exercises

--- 10. EXERCISE INTERFACES AND ABSTRACTION/src/task4_Telephony/Config.java
+++ 10. EXERCISE INTERFACES AND ABSTRACTION/src/task4_Telephony/Config.java
@@ -2,12 +2,12 @@ package task4_Telephony;
 
 import java.util.regex.Pattern;
 
-public class Config {
-    public static final Pattern PHOENUMBER_PATTERN = Pattern.compile("[a-zA-Z]+");
-    public static final String INVALID_PHONENUMBER = "Invalid number!";
-    public static final String CALLING_NUMBER = "Calling... %s";
+class Config {
+    static final Pattern PHOENUMBER_PATTERN = Pattern.compile("[a-zA-Z]+");
+    static final String INVALID_PHONENUMBER = "Invalid number!";
+    static final String CALLING_NUMBER = "Calling... %s";
 
-    public static final Pattern WEBSITE_PATTERN = Pattern.compile("[0-9]+");
-    public static final String INVALID_WEBSITE = "Invalid URL!";
-    public static final String BROWSING_WEBSITE = "Browsing: %s!";
+    static final Pattern WEBSITE_PATTERN = Pattern.compile("[0-9]+");
+    static final String INVALID_WEBSITE = "Invalid URL!";
+    static final String BROWSING_WEBSITE = "Browsing: %s!";
 }
