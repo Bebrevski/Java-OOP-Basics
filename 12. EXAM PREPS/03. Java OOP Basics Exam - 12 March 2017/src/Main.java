184bfc6f3c59e76a1234e6cb9c1188707f3219fe
Bebrevski <stefan.bebrevski89@gmail.com>
2018-07-06 21:05:01 +0300

    Course exercises

--- 12. EXAM PREPS/03. Java OOP Basics Exam - 12 March 2017/src/Main.java
+++ 12. EXAM PREPS/03. Java OOP Basics Exam - 12 March 2017/src/Main.java
@@ -1,8 +1,22 @@
-import controller.CarManager;
-import view.IOManager;
+package avatar;
 
+import core.CarManager;
+import engines.Engine;
+import io.ConsoleInputReader;
+import io.ConsoleOutputWriter;
+import utilities.InputParser;
+
+/**
+ * Created by Hristo Skipernov on 08/05/2017.
+ */
 public class Main {
     public static void main(String[] args) {
-        IOManager.getInstance().run(CarManager.getInstance());
+        ConsoleInputReader inputReader = new ConsoleInputReader();
+        ConsoleOutputWriter outputWriter = new ConsoleOutputWriter();
+        InputParser inputParser = new InputParser();
+        CarManager carManager = new CarManager();
+        Engine engine = new Engine(inputReader, outputWriter, inputParser, carManager);
+
+        engine.run();
     }
 }
